6e2265ec2698c6d3f7530720ffc80f262c4c607c
Bryan Schlief <bschlief@gmail.com>
2012-08-02 18:05:56 -0700

    Fix Level 3

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ My implementation of the Ruby Warrior project to learn ruby.
 Aug 2
 * Level 1
 * Level 2
+* Level 3
--- bschlief-beginner/player.rb
+++ bschlief-beginner/player.rb
@@ -1,10 +1,31 @@
 class Player
+  attr_accessor :need_heal
+
   def play_turn(warrior)
-    # add your code here
+
+    if warrior.health == 20
+      @need_heal = false
+    end
+
+    if warrior.health < 6
+      @need_heal = true
+    end
+
+    if @need_heal && !warrior.feel.empty?
+      return warrior.walk!(:backward)
+    end
+
+    if @need_heal && warrior.health < 20
+      return warrior.rest!
+    end
+
     if !warrior.feel.empty?
       return warrior.attack! 
     end
 
     warrior.walk!
   end
+
+
 end
+
