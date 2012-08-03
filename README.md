b04cdd077aac85d1506b9321232de087cabe60f5
Bryan Schlief <bschlief@gmail.com>
2012-08-02 17:45:48 -0700

    Fix Level 2. Attack sludge if ahead.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ rubywarrior
 ===========
 
 My implementation of the Ruby Warrior project to learn ruby.
+
+Aug 2
+* Level 1
+* Level 2
--- bschlief-beginner/player.rb
+++ bschlief-beginner/player.rb
@@ -1,6 +1,10 @@
 class Player
   def play_turn(warrior)
     # add your code here
+    if !warrior.feel.empty?
+      return warrior.attack! 
+    end
+
     warrior.walk!
   end
 end
